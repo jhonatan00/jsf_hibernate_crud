ed0c20f021cdc331dad40cf4317a5e3035cf6f3a
Fabio Alves <fabio.a.gomes@bol.com.br>
2014-03-11 07:32:30 -0300

    First commit

---
+++ src/dao/LivroDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.Livro;
+import java.util.List;
+ 
+public interface LivroDao {
+ 
+public void save(Livro livro);
+public Livro getLivro(long id);
+public List<Livro> list();
+public void remove(Livro livro);
+public void update(Livro livro);
+ 
+}
